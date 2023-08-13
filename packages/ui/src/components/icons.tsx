5e2e70c2d6e86bc961521db756aa900baea4f06d
Rickard Lindahl <rickard@lindahl.app>
2023-08-13 09:14:46 +0200

    Add icons to ui package

---
+++ packages/ui/src/components/icons.tsx
@@ -0,0 +1,22 @@
+import {
+  Command,
+  Loader2,
+  Menu,
+  Moon,
+  SunMedium,
+  User,
+  X,
+  type LucideIcon,
+} from "lucide-react";
+
+export type Icon = LucideIcon;
+
+export const Icons = {
+  close: X,
+  logo: Command,
+  menu: Menu,
+  moon: Moon,
+  spinner: Loader2,
+  sun: SunMedium,
+  user: User,
+};
